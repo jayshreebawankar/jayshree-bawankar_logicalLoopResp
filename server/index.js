113d7b531d71d0b035cfb0b2f9b82a9ae7cc1ad7
jayshreebawankar <98347627+jayshreebawankar@users.noreply.github.com>
2022-03-01 13:42:24 +0530

    project has been added

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const UserRouter = require("./routes/UserRouter.js");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/user", UserRouter);
+
+mongoose
+  .connect(
+    `mongodb+srv://roshanadmin:roshanmongo@cluster0.zmwyl.mongodb.net/Todo?retryWrites=true&w=majority`
+  )
+  .then(
+    app.listen("8000", () => {
+      console.log("Server runs at port 8000");
+    })
+  )
+  .catch((err) => {
+    console.log(err);
+  });
